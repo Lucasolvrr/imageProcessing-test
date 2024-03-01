063e61f61539dceb39a2cc651a7e5ab94454acd6
Lukas-xrze <lukasoliveiracks@gmail.com>
2024-03-01 18:27:55 -0300

    feature: resize option added

--- guassianBlur.py
+++ guassianBlur.py
@@ -4,9 +4,12 @@ import cv2
 image = cv2.imread('images/image.jpg')
 
 # Aplicar filtro gaussiano
-blurred = cv2.GaussianBlur(image, (5, 5), 0)  # kernel de 5x5, desvio padrão 0
+blurred = cv2.GaussianBlur(image, (5, 5), 5)  
 
 # Mostrar imagens
+cv2.namedWindow('Original', cv2.WINDOW_NORMAL)    
+cv2.namedWindow('Suavizada (Gaussiano)', cv2.WINDOW_NORMAL)    
+
 cv2.imshow('Original', image)
 cv2.imshow('Suavizada (Gaussiano)', blurred)
 cv2.waitKey(0)
